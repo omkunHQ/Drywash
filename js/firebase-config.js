c18a7fc213b587945227f4788e44b9dd075b59e4
omkunHQ <omkunhq@gmail.com>
2025-10-24 17:45:52 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,22 +2,43 @@
 
 // --- Step 1: Firebase SDKs Import Karein ---
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { 
-    getFirestore, Timestamp, collection, addDoc, getDocs, onSnapshot, 
-    doc, updateDoc, deleteDoc, setDoc, getDoc, getCountFromServer, 
-    query, where, orderBy, serverTimestamp, limit 
+    getAuth, 
+    onAuthStateChanged, 
+    signOut,
+    createUserWithEmailAndPassword, // <-- YEH ADD KAREIN (Signup ke liye)
+    signInWithEmailAndPassword    // <-- YEH ADD KAREIN (Login ke liye)
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { 
+    getFirestore, 
+    Timestamp, 
+    collection, 
+    addDoc, 
+    getDocs, 
+    onSnapshot, 
+    doc, 
+    updateDoc, 
+    deleteDoc, 
+    setDoc, 
+    getDoc, 
+    getCountFromServer, 
+    query, 
+    where, 
+    orderBy, 
+    serverTimestamp, 
+    limit,
+    writeBatch                  // <-- YEH ADD KAREIN (Default products ke liye)
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // --- Step 2: Apna Firebase Config Paste Karein ---
 const firebaseConfig = {
-    apiKey: "AIzaSyCxVmGqlbomo47KqXZm4S8QqfL3bXZN6pg",
-    authDomain: "drywash-7d086.firebaseapp.com",
-    projectId: "drywash-7d086",
-    storageBucket: "drywash-7d086.firebasestorage.app",
-    messagingSenderId: "850083946512",
-    appId: "1:850083946512:web:f0ae239d283abd1dffaa95",
-    measurementId: "G-TLPBTQ31KT"
+    apiKey: "AIzaSyCxVmGqlbomo47KqXZm4S8QqfL3bXZN6pg",
+    authDomain: "drywash-7d086.firebaseapp.com",
+    projectId: "drywash-7d086",
+    storageBucket: "drywash-7d086.firebasestorage.app",
+    messagingSenderId: "850083946512",
+    appId: "1:850083946512:web:f0ae239d283abd1dffaa95",
+    measurementId: "G-TLPBTQ31KT"
 };
 
 // --- Step 3: Firebase Services Initialize Karein ---
@@ -36,6 +57,8 @@ export {
     // Auth
     onAuthStateChanged, 
     signOut,
+    createUserWithEmailAndPassword, // <-- YEH EXPORT KAREIN
+    signInWithEmailAndPassword,   // <-- YEH EXPORT KAREIN
     
     // Firestore
     Timestamp, 
@@ -53,5 +76,6 @@ export {
     where, 
     orderBy, 
     serverTimestamp, 
-    limit
+    limit,
+    writeBatch                  // <-- YEH EXPORT KAREIN
 };
